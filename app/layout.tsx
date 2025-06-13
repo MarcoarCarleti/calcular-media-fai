89494bc78e8756290420b15d4b720929c398a6d4
MarcoarCarleti <marcarleti@gmail.com>
2025-06-13 14:07:57 -0300

    fix: ads config

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,17 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-      <meta
-        name="google-adsense-account"
-        content="ca-pub-3195296029072717"
-      />
-      <GoogleAdsense pId="3195296029072717" />
-    </head>
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-3195296029072717" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3195296029072717"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={`${inter.className} dark`}>
         {children}
         <Analytics />
-        
       </body>
     </html>
   );
